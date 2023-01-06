4e0deaa9edcdf83b30e4883fb7a082315d0a66ae
UlanAkun <ulanbek7770@gmail.com>
2023-01-06 15:58:55 +0600

    novoe prilojenie

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:to_do_app/home_page.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  var box = await Hive.openBox('mybox');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: HomePage(),
+    );
+  }
+}
